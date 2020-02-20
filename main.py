de0dd8d68af8c773c43a75afca29c8f2a94aed71
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-02-20 11:56:46 -0500

    add argparse and vis option to main

--- main.py
+++ main.py
@@ -8,28 +8,51 @@ from model import Model
 import json
 import os
 from sklearn.metrics import confusion_matrix, accuracy_score
-
-dataObj = DataObj("/home/nv3le/workspace/Doping/PySpacerSolver/Exp55/ind_gen_files")
-train = dataObj.train
-test = dataObj.test
-vocab = dataObj.vocab
-
-def evaluate(model, testset):
+from termcolor import colored
+import argparse
+def evaluate(model, testset, vis = False):
     output = model(
         testset["C_batch"],
         testset["L_a_batch"],
         testset["L_b_batch"]
     ).cpu()
     true_label = testset["label_batch"].cpu()
-    print("true:", testset["label_batch"])
+    # print("true:", testset["label_batch"])
+    m = nn.Softmax(dim = 1)
     # print("output:", output)
-    values, pred = torch.max(output, 1)
-    print("pred:", pred)
+    values, pred = torch.max(m(output), 1)
+
+    # print("pred:", pred)
     acc = accuracy_score(true_label, pred)
     print(confusion_matrix(true_label, pred))
     print("accurarcy", acc)
+
+    true_label = true_label.tolist()
+    values = values.tolist()
+    pred = pred.tolist()
+    if vis:
+        print("Label\tPred\tConfidence")
+        for i in range(len(pred)):
+            if true_label[i]!=pred[i]:
+                print(colored("%d\t%d\t%f"%(true_label[i], pred[i], values[i]), 'red'))
+            else:
+                print(colored("%d\t%d\t%f"%(true_label[i], pred[i], values[i]), 'green'))
 if __name__ == '__main__':
-    model = Model(vocab['size'], emb_dim = 20, tree_dim = 20).train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-input', help='path to the ind_gen_files folder')
+    parser.add_argument("-l", "--log", dest="logLevel", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='CRITICAL', help="Set the logging level")
+    parser.add_argument('-vis', action='store_true')
+    args = parser.parse_args()
+
+    exp_folder = args.input
+    vis = args.vis
+    dataObj = DataObj(exp_folder)
+    train = dataObj.train
+    test = dataObj.test
+    vocab = dataObj.vocab
+
+
+    model = Model(vocab['size'], vocab['sort_size'], emb_dim = 32, tree_dim = 200, out_dim =2).train()
     loss_function = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters())
     print("Training with %d datapoints"%train["size"])
@@ -51,6 +74,6 @@ if __name__ == '__main__':
             #check that embedding is being trained
             print(model.emb(torch.LongTensor([5]).to(device = torch.device('cuda') ) ) )
             print("training eval---------------------------")
-            evaluate(model, train)
+            evaluate(model, train, vis)
             print("testing eval----------------------------")
-            evaluate(model, test)
+            evaluate(model, test, vis)
