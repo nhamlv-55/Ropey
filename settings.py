707fed62756b11af7153dc514d54df4785b97df9
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-04-06 12:40:17 -0400

    forgot to save settings before commit

--- settings.py
+++ settings.py
@@ -1,8 +1,12 @@
 import datetime
+import os
 MODEL_PATH='models'
 
 def new_model_name(basename):
     x = datetime.datetime.now()
     return "%s-%s"%(basename, str(x))
 
-
+def new_model_path(exp_path):
+    basename = os.path.basename(exp_path)
+    new_model_name = new_model_name(basename)
+    new_model_path(os.path.join(MODEL_PATH), basename)
