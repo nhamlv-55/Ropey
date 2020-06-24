44eac01948fc68790ecc663d6235ad51ebd39403
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-06-24 11:02:46 -0400

    clean up X_eval

--- X_eval.py
+++ X_eval.py
@@ -7,16 +7,8 @@ def setup_model(model_path):
     checkpoint = torch.load(model_path)
     model_metadata = checkpoint['metadata']
     dataset_metadata = checkpoint['dataset']
+    print("Epoch", checkpoint["epoch"])
 
-    # model = Model(dataset_metadata['vocab_size'],
-    #               dataset_metadata['sort_size'],
-    #               emb_dim = model_metadata['emb_dim'],
-    #               tree_dim = model_metadata['tree_dim'],
-    #               out_dim = 2,
-    #               use_c = model_metadata['use_c'],
-    #               use_const_emb = model_metadata['use_const_emb'],
-    #               use_dot_product = model_metadata['use_dot_product']
-    # ).eval()
     model = Model(dataset_metadata['vocab_size'],
                   dataset_metadata['sort_size'],
                   emb_dim = 30, #30 is the max emb_dim possible, due to the legacy dataset
@@ -27,47 +19,6 @@ def setup_model(model_path):
 
     return model
 
-def predict_core(model, lit_a, lit_b):
-    """
-    return P(lit_a|lit_b)
-    """
-    output = model(
-        test["L_a_batch"],
-        test["L_b_batch"]
-    )
-
-    pass
-
-
-def run(model, dataObj, model_metadata):
-    test, last_batch = dataObj.next_batch(dataObj.test_dps, "test")
-    L_a_tree = test["L_a_batch"]
-    L_b_tree = test["L_b_batch"]
-    output = model(test["C_batch"], test["L_a_batch"], test["L_b_batch"])
-
-    json_vis_data = {}
-    json_vis_data["filename"] = test["filenames"]
-    json_vis_data["node_a"] = list(range(L_a_tree['tree_sizes'][0]))
-    json_vis_data["adj_list_a"] = L_a_tree["adjacency_list"].tolist()
-
-    json_vis_data["node_b"] = list(range(L_b_tree['tree_sizes'][0]))
-    json_vis_data["adj_list_b"] = L_b_tree["adjacency_list"].tolist()
-
-    json_vis_data["h_a_raw"] = output[1]["h_a_raw"].tolist()
-    json_vis_data["h_a_min"] = torch.min(output[1]["h_a_raw"], dim = 0)[0].tolist()
-    json_vis_data["h_a_max"] = torch.max(output[1]["h_a_raw"], dim = 0)[0].tolist()
-
-    json_vis_data["h_b_raw"] = output[1]["h_b_raw"].tolist()
-    json_vis_data["h_b_min"] = torch.min(output[1]["h_b_raw"], dim = 0)[0].tolist()
-    json_vis_data["h_b_max"] = torch.max(output[1]["h_b_raw"], dim = 0)[0].tolist()
-
-    json_vis_data["vocab"] = dataObj.vocab
-    json_vis_data["feature_a"] = L_a_tree["features"].tolist()
-    json_vis_data["feature_b"] = L_b_tree["features"].tolist()
-    json_vis_data["metadata"] = model_metadata
-    return json_vis_data
-
-
 if __name__=="__main__":
     torch.no_grad()
     parser = argparse.ArgumentParser()
@@ -75,5 +26,5 @@ if __name__=="__main__":
     args = parser.parse_args()
 
     model_path = args.model_path
-    model, dataObj, model_metadata = setup_model(model_path)
+    model = setup_model(model_path)
     # run(model, dataObj, model_metadata)
