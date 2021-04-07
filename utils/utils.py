d87c13e2630627cc6b0d00c53a524e6ae2fc9e89
Nham Le <nv3le@uwaterloo.ca>
2021-04-07 00:53:34 -0400

    refactor code to write to tfboard

--- utils/utils.py
+++ utils/utils.py
@@ -135,3 +135,32 @@ def parser_from_template(json_config_template = "/home/nle/workspace/Doping/exp_
                                 type = arg_type,
                                 help = help_text)
     return parser
+
+def plot_to_tensorboard(writer, fig, step):
+    """
+    Takes a matplotlib figure handle and converts it using
+    canvas and string-casts to a numpy array that can be
+    visualized in TensorBoard using the add_image function
+
+    Parameters:
+        writer (tensorboard.SummaryWriter): TensorBoard SummaryWriter instance.
+        fig (matplotlib.pyplot.fig): Matplotlib figure handle.
+        step (int): counter usually specifying steps/epochs/time.
+    """
+
+    # Draw figure on canvas
+    fig.canvas.draw()
+
+    # Convert the figure to numpy array, read the pixel values and reshape the array
+    img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    # Normalize into 0-1 range for TensorBoard(X). Swap axes for newer versions where API expects colors in first dim
+    img = img / 255.0
+    # img = np.swapaxes(img, 0, 2) # if your TensorFlow + TensorBoard version are >= 1.8
+
+    # Add figure in numpy "image" to TensorBoard writer
+    writer.add_image('confusion_matrix', img, step)
+    plt.close(fig)
+
+
